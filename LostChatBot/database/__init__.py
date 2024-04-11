700cdbf23b1dbc74bac3c8dc9df47ca64728df00
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-04-11 00:53:36 -0700

    Update __init__.py

--- LostChatBot/database/__init__.py
+++ LostChatBot/database/__init__.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 import config
 
-vickdb = MongoClient(config.MONGO_URL)
-vick = LostChatBot["LostChatBot"]["LostChatBot"]
+lostdb = MongoClient(config.MONGO_URL)
+lost = lostdb["LostDb"]["Lost"]
 
 from .chats import *
 from .users import *
